a1e86db64d10140efc7184c799f462895fa5939a
Denis Prusakov <snaiper.arts@gmail.com>
2022-04-07 00:29:31 +0400

    Lesson #1

--- backend_drf/users/models.py
+++ backend_drf/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class UserInfo(AbstractUser):
     email = models.EmailField(unique=True)
     is_manage = models.BooleanField('Менеджер проекта', default=False)
